0b08843991f804eb17ee8e2f36ca2f2d906c1a84
Kumar <yogesh.1695@gmail.com>
2018-01-17 22:53:53 +0530

    debugged

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -65,7 +65,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # methodical)
         self.browser.implicitly_wait(3)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('2: Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
